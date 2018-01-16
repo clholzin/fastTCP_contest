558b353891436bba6ce3d169d3c4ff19031b02aa
Craig Holzinger <craig.holzinger@aexp.com>
2018-01-16 08:30:41 -0700

    rework tcp

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"os/signal"
 	"sort"
 	"strings"
-	"time"
-	"io/ioutil"
-	"os/signal"
 	"sync"
-	"io"
+	"time"
+	"bufio"
 )
 
 const (
@@ -25,48 +26,57 @@ const (
 )
 
 type (
-	count    int
-	Duration int
-	File     *os.File
+	count           int
+	Duration        int
+	File            *os.File
 	incomingBufData []byte
-	counter  struct {
-		sync.Mutex
-		total      int
-		totalSince int
-		totalUniq  int
-		duplicates int
-		fileCount  int
-		currentMap map[string]int
-		memoizeFile func()(*os.File)
+	counter         struct {
+		mu          sync.Mutex
+		total       int
+		totalSince  int
+		totalUniq   int
+		duplicates  int
+		fileCount   int
+		currentMap  map[string]int
+		memoizeFile func() *os.File
 	}
 )
 
 var (
-	connCounter   count
+	connCounter count
 	//inputRecievedCounter int
-	keptValuesBuf bytes.Buffer
+	//keptValuesBuf bytes.Buffer
+	connSubtract = make(chan int,1)
 	//gCachInputMap bytes.Buffer
-	err           error
+	//err error
 )
 
-
-
-func (c *counter) dumpCounts () {
-
+func (c *counter) dumpCounts() {
 	c.total = 0
 	c.totalSince = 0
 	c.totalUniq = 0
 	//c.duplicates = 0
+	/*file := c.memoizeFile()
+	file.Close()
+	fileLog, err := createLog(c.fileCount)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	c.memoizeFile = func() (*os.File) {
+		return fileLog
+	}*/
 	c.currentMap = make(map[string]int)
 }
 
-func init(){
+func init() {
 	cleanLogs()
 }
 
 func main() {
 	// Listen for incoming connections.
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+CONN_PORT)
+	fmt.Println("Listening on " + CONN_HOST + CONN_PORT)
 	mainCounter := new(counter)
 	mainCounter.currentMap = make(map[string]int)
 	c := make(chan os.Signal, 1)
@@ -76,19 +86,19 @@ func main() {
 		os.Exit(1)
 	}
 	mainCounter.interval5()
-	mainCounter.interval10()
-	fileLog,err := createLog(mainCounter.fileCount)
+
+	/*fileLog, err := createLog(mainCounter.fileCount)
 	if err != nil {
-		fmt.Println(fmt.Sprintf("Error failed opening data.%d.log:\n %s \n",mainCounter.fileCount, err.Error()))
+		fmt.Println(fmt.Sprintf("Error failed opening data.%d.log:\n %s \n", mainCounter.fileCount, err.Error()))
 		return
 	}
-	mainCounter.memoizeFile = func()(f *os.File){
-		f = fileLog
-		return f
-	}
+	mainCounter.memoizeFile = func() (*os.File) {
+		return fileLog
+	}*/
+	mainCounter.interval10()
 	// Close the listener when the application closes.
 	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST+CONN_PORT)
+
 	for {
 		// Listen for an incoming connection.
 		conn, err := l.Accept()
@@ -96,140 +106,167 @@ func main() {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
-		go func(){
+		if connCounter >= 6 {
+			conn.Close()
+			continue
+		}
+		connCounter++
+		currentCounter    := new(counter)
+		//currCountChan     := make(chan *counter,1)
+		currentCounter.currentMap = make(map[string]int)
+		lineBreakByteBuf  := incomingBufData("\n")
+		var drainBuf      = make(incomingBufData, 1024)
+		var keptValuesBuf = bytes.NewBuffer(drainBuf)
 
+
+		go func() {
 			defer conn.Close()
-
-			if connCounter == 6 {
-				return
-			}
-
-			connCounter++
-
-			lineBreakByteBuf   := incomingBufData("\n")
-			var drainBuf       =  make(incomingBufData,10)
-			var keptValuesBuf  =  bytes.NewBuffer(drainBuf)
-			var data 		   =  make(incomingBufData,10)
-			var incomingBuf    =  bytes.NewBuffer(data)
+			bufReader := bufio.NewReader(conn)
+			//for {
+				//var data  = make(incomingBufData, 1024)
+				//var incomingBuf   = bytes.NewBuffer(data)
+				//bytesRead, err := conn.Read(data[:])
+				//if bytesRead == 0 || err != nil{
+				//	return
+				//}
 
 				for {
-					if _, err = conn.Read(data[:]); err != nil {
+					//sValue, err := incomingBuf.ReadString(lineBreakByteBuf[0])
+					byteValue,err := bufReader.ReadBytes(lineBreakByteBuf[0])
+					if err != nil || byteValue[0] == lineBreakByteBuf[0]{
+						if err != io.EOF {
+							fmt.Println(err.Error())
+						}
 						return
 					}
-
-					for {
-						sValue,err := incomingBuf.ReadString(lineBreakByteBuf[0])
-						if err != nil && err != io.EOF {
-						    fmt.Println(err.Error())
-						}
-
-						if len(sValue) > 0 && sValue != "EOF"  {
-							mainCounter.total++
-							switch {
-							case len(sValue) == 0 || sValue == "\n" || sValue == "\r":
-								fallthrough
-							case strings.ToLower(sValue) == APP_SHUTDOWN:
-								signal.Notify(c, os.Interrupt)
-								break
-							default:
-								sValue = strings.Replace(sValue,"\n","",1)
-								checkVal := checkLeadingZeros(sValue)
-								if len(checkVal) > 6 {
-									mainCounter.Lock()
-									if mainCounter.currentMap[checkVal] == 0 {
-										keptValuesBuf.Write(append(incomingBufData(checkVal),lineBreakByteBuf[0]))
-										mainCounter.currentMap[checkVal] = 1
-									}else{
-										mainCounter.currentMap[checkVal]++
-										mainCounter.duplicates++
-									}
-									mainCounter.Unlock()
+					sValue := string(byteValue[:])
+					if len(sValue) > 0 && sValue != "\n" && sValue != "\r" {
+						mainCounter.total++
+						switch {
+						case strings.ToLower(sValue) == APP_SHUTDOWN:
+							signal.Notify(c, os.Interrupt)
+							return
+						default:
+							sValue = strings.Replace(sValue, "\n", "", 1)
+							checkVal := checkLeadingZeros(sValue)
+							if len(checkVal) > 5 {
+								if currentCounter.currentMap[checkVal] == 0 {
+									keptValuesBuf.Write(append(incomingBufData(checkVal), lineBreakByteBuf[0]))
+									currentCounter.currentMap[checkVal] = 1
+								} else {
+									currentCounter.currentMap[checkVal]++
+									currentCounter.duplicates++
 								}
 							}
 						}
-						if err != nil && err == io.EOF {
-							break
-						}
+					}else{
+						return
 					}
+					/*else if err != nil && err == io.EOF {
+						break
+					}*/
 				}
-			    fileLog := mainCounter.memoizeFile()
-				if keptValuesBuf.Len() > 0 {
-					if _,err = fileLog.Write(keptValuesBuf.Bytes()); err != nil{
-						fmt.Println(err.Error())
-					}
-				}
-
-			if connCounter > 0 {
-				connCounter--
-			}
+			//}
+		//	currCountChan <- currentCounter
+		//	connSubtract <- 1
 
 			select {
 			case <-c:
 				os.Exit(1)
+			default:
 			}
+
+
+			//select {
+			//case currentCounts := <-currCountChan:
+			mainCounter.mu.Lock()
+			//fileLog := mainCounter.memoizeFile()
+			fileLog, err := createLog(mainCounter.fileCount)
+			if err != nil{
+				fmt.Println(err)
+			}
+			if keptValuesBuf.Len() > 0 {
+				if _, err = fileLog.Write(bytes.Trim(keptValuesBuf.Bytes(),"")); err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+			fileLog.Close()
+			mainCounter.total += currentCounter.total
+			mainCounter.duplicates += currentCounter.duplicates
+			for k,_ := range currentCounter.currentMap {
+				if mainCounter.currentMap[k] == 0 {
+					mainCounter.currentMap[k] = currentCounter.currentMap[k]
+				}else{
+					mainCounter.currentMap[k] += currentCounter.currentMap[k]
+				}
+			}
+			mainCounter.mu.Unlock()
+			//}
 		}()
+
 
 	}
 }
 
-
 func (c *counter) interval5() {
 	//do read out for uniq numbers in this interval of 10 seconds
 	// do read out for uniq numbers total for server duration
-	 go func(){
-	    c.Lock()
-		//if gCachInputMap.Len() > 0 {
+	go func() {
+		c.mu.Lock()
 		c.countAllLogs()
-	    fmt.Printf("Total unique numbers this session: %d" +
-	    	"\nTotal unique numbers: %d" +
-			"\nTotal duplicates received: %d" +
-	    	"\nTotal Recieved %d\n\n",
-	    		c.totalSince,c.totalUniq,c.duplicates,c.total)
+		fmt.Printf("Total unique numbers this session: %d"+
+			"\nTotal unique numbers: %d"+
+			"\nTotal duplicates received: %d"+
+			"\nTotal Recieved %d\n\n",
+			c.totalSince, c.totalUniq, c.duplicates, c.total)
 		c.dumpCounts()
-	    c.Unlock()
+		c.mu.Unlock()
 		time.Sleep(5 * time.Second)
-		// do the work
-		 c.interval5()
-	 }()
+		c.interval5()
+	}()
 }
 
 func (c *counter) interval10() {
 	//Every 10 seconds, the log should rotate and increment the number in the name,
 	//all while only writing unique numbers. Example: data.0.log -> data.1.log -> data.2.log.
-	 go func(){
-		 c.Lock()
-		c.fileCount++//count and create new file
-		fileLog,err := createLog(c.fileCount)
-	    c.memoizeFile = func()(f *os.File){
-	    	f = fileLog
-		   return f
-	    }
-		 c.Unlock()
+	go func() {
+		c.mu.Lock()
+		c.fileCount++ //count and create new file
+		/*file := c.memoizeFile()
+		file.Close()
+		fileLog, err := createLog(c.fileCount)
 		if err != nil || c.fileCount > 10 {
 			return
 		}
+		c.memoizeFile = func() (*os.File) {
+			return fileLog
+		}*/
+		_, err := createLog(c.fileCount)
+		if err != nil || c.fileCount > 10 {
+			return
+		}
+		c.mu.Unlock()
+
 		time.Sleep(10 * time.Second)
 		c.interval10()
-	 }()
+	}()
 
 }
 
 func checkLeadingZeros(s string) (t string) {
 	valueLen := len(s)
 	var zeroByte = []byte("0")
-	for i := 0; i<valueLen;i++ {
+	i := 0
+	for ; i < valueLen; i++ {
 		if s[i] == zeroByte[0] {
-			if i+1 < len(s) {
-				s = s[i+1:]
-			} else {
-				s = s[i:]
+			if i+1 >= len(s) {
 				break
 			}
-		}else{
+		} else {
 			break
 		}
 	}
-	t = s
+	t = s[i:]
 	return t
 }
 
@@ -248,15 +285,15 @@ func readDirNames(dirname string) ([]string, error) {
 	return names, nil
 }
 
-func createLog(fileCount int) (f *os.File,err error){
+func createLog(fileCount int) (f *os.File, err error) {
 	// create initial data log
-	fileName  := fmt.Sprintf("data.%d.log",fileCount)
+	fileName := fmt.Sprintf("data.%d.log", fileCount)
 	f, err = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
-		fmt.Println("Failed to open file: ",err.Error())
-		return nil,err
+		fmt.Println("Failed to open file: ", err.Error())
+		return nil, err
 	}
-	return f,err
+	return f, err
 }
 
 func cleanLogs() {
@@ -277,7 +314,7 @@ func cleanLogs() {
 
 }
 
-func (c *counter)countAllLogs() {
+func (c *counter) countAllLogs() {
 	files, err := readDirNames(APP_BASE_PATH)
 	if err != nil {
 		log.Fatal(err)
@@ -285,19 +322,19 @@ func (c *counter)countAllLogs() {
 	for _, value := range files {
 
 		if strings.Contains(value, "data.") {
-			logFile,err := os.Open(value)
+			logFile, err := os.Open(value)
 			if err != nil {
 				fmt.Println(err)
-				return
+				continue
 			}
-			data,err := ioutil.ReadAll(logFile)
+			data, err := ioutil.ReadAll(logFile)
 			if err != nil {
 				fmt.Println(err)
-				return
+				continue
 			}
-			if strings.Contains(value,string(c.fileCount)) {
-				c.totalSince =  bytes.Count(data, []byte("\n"))
-			}else{
+			if strings.Contains(value, string(c.fileCount)) {
+				c.totalSince += bytes.Count(data, []byte("\n"))
+			} else {
 				c.totalUniq += bytes.Count(data, []byte("\n"))
 			}
 
