bf09019164c8ba68891edace9eb995fbb849f31e
Craig Holzinger <craig.holzinger@aexp.com>
2018-01-18 22:43:58 -0700

    working copy Total unique numbers this session: 14784273 Total unique numbers: 1592088 Total duplicates received: 15 Total Recieved 16376355

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ type (
 		countData *counter
 		readData  []byte
 	}
+	triggerChan chan int
 )
 
 var (
@@ -69,9 +70,9 @@ func main() {
 	mainCounter := new(counter)
 	mainCounter.currentMap = make(map[string]int)
 	killChan := make(chan os.Signal, 1)
-
-	mainCounter.interval10()
-	mainCounter.interval5()
+	triggerInterval5Chan := make(triggerChan, 6)
+	go mainCounter.interval10()
+	go mainCounter.interval5(&connCounter,triggerInterval5Chan)
 
 	// Close the listener when the application closes.
 	defer l.Close()
@@ -91,7 +92,7 @@ func main() {
 
 			countChan <- 1
 
-			go pump(conn, killChan, backendChannel, countChan)
+			go pumpData(conn, killChan, backendChannel, countChan,triggerInterval5Chan)
 
 		}
 	}()
@@ -113,101 +114,87 @@ func main() {
 	}
 }
 
-func pump(connection net.Conn, c chan os.Signal, backendChannel chan *bundleData, countChan chan count) {
+func pumpData(connection net.Conn,c chan os.Signal,backendChannel chan *bundleData,
+	countChan chan count,triggerInterval5Chan triggerChan) {
+
 	defer func() {
-		connection.Close()
+		//connection.Close()
 		countChan <- -1 // sync with chan
 	}()
 
 	sValue := ""
 	currentCounter := new(counter)
 	currentCounter.currentMap = make(map[string]int)
-	lineBreakByteBuf := incomingBufData("\n")
+	//lineBreakByteBuf := incomingBufData("\n")
 	killLoopChan := make(chan int, 1)
 	drainBuf := make(incomingBufData, 1024)
 	keptValuesBuf := bytes.NewBuffer(drainBuf)
 	bu := make([]byte, 1024)
-	bufConn := bytes.NewBuffer(bu)
-
 	for {
-		_, err := connection.Read(bu) //bufio.NewReader(connection)
-		//bytesLeft := bufReader.Buffered()
-		//readString := string(bu[:])
+		_, err := connection.Read(bu)
 		if err != nil {
 			fmt.Println(err.Error())
 			break
 		}
-		readString := string(bu[:])
+		bufConn := bytes.NewBuffer(bu)
+		readString := bufConn.String()
+		if len(readString) == 0 {
+			break
+		}
+		currentCounter.mu.Lock()
+		currentCounter.currentMap = make(map[string]int)
+		currentCounter.mu.Unlock()
+
 		sValueArray := strings.Split(readString, "\n")
 		for i := 0; i < len(sValueArray); i++ {
 			sValue = sValueArray[i]
 			if len(sValue) == 0 {
 				continue
 			}
-			/*sValue,err = bufConn.ReadString(lineBreakByteBuf[0])
-			if err != nil || sValue == ""{
-				if sValue == "ooopps"{
-					fmt.Println(sValue)
-					return
-				}
-				sValue = "ooopps"
-				break
-			}*/
-			//if bytesLeft > 0 {
-			//bufReader.ReadBytes(lineBreakByteBuf[0])
-			/*if err != nil || len(sValue) == 0{//|| byteValue[0] == lineBreakByteBuf[0]
-				if err != io.EOF {
-					fmt.Println(err.Error())
-					return
-				}
-				killLoopChan <- 1
-			}*/
-			/*if len(sValue) < 5 {
-				continue
-			}*/
-			//sValue := string(byteValue[:])
 			currentCounter.total++
+			currentCounter.totalSince++
 			switch {
 			case strings.ToLower(sValue) == APP_SHUTDOWN:
-				signal.Notify(c, os.Interrupt)
-				return
-			default: //len(sValue) > 0 && sValue != "\n" && sValue != "\r"
-				//sValue = strings.Replace(sValue, "\n", "", 1)
-				checkVal := sValue //checkLeadingZeros(sValue)
-				/*if len(checkVal) > 5 {
-					if currentCounter.currentMap[checkVal] == 0 {
-						keptValuesBuf.Write(append(incomingBufData(checkVal), lineBreakByteBuf[0]))
-						currentCounter.currentMap[checkVal] = 1
-						currentCounter.totalUniq++
-					} else {
-						currentCounter.currentMap[checkVal]++
-						currentCounter.duplicates++
-					}
-				}*/
-				keptValuesBuf.Write(append(incomingBufData(checkVal), lineBreakByteBuf[0]))
-				if currentCounter.currentMap[checkVal] == 0 {
+				signal.Notify(c, os.Interrupt)// receive shutdown string and kill app
+				break
+			default:
+				//checkVal := checkLeadingZeros(sValue)
+				if _,ok := currentCounter.currentMap[sValue]; !ok {
 					currentCounter.totalUniq++
+					keptValuesBuf.Write(incomingBufData(sValue+"\n"))//append(incomingBufData(sValue), lineBreakByteBuf[0])
+				}else{
+					currentCounter.duplicates++
 				}
-				currentCounter.currentMap[checkVal]++
+				currentCounter.currentMap[sValue]++
 			}
-
-			//}
 			select {
 			case <-killLoopChan:
 				break
+			case <-triggerInterval5Chan:
+				u := keptValuesBuf.Bytes()
+				/* clone map */
+				nmap := make(map[string]int)
+				for g,m := range currentCounter.currentMap {
+					nmap[g] = m
+				}
+				newCountCurrent := new(counter)
+				newCountCurrent.total = currentCounter.total
+				newCountCurrent.duplicates = currentCounter.duplicates
+				newCountCurrent.totalUniq = currentCounter.totalUniq
+				newCountCurrent.totalSince = currentCounter.totalSince
+				newCountCurrent.currentMap = nmap
+				b := &bundleData{newCountCurrent, append([]byte(""),u...)}
+				backendChannel <- b
+				keptValuesBuf.Reset()
 			default:
+
 			}
 		}
-		bufConn.Reset()
-	}
-	b := &bundleData{currentCounter, keptValuesBuf.Bytes()}
-	backendChannel <- b
 
-	/*select {
-	case <-c:
-		os.Exit(1)
-	default:
-	}*/
+		 bufConn.Reset()
+		 bufConn.Grow(1024)
+	}
+
 
 }
 
@@ -215,17 +202,19 @@ func (c *counter) consume(backendChannel chan *bundleData) {
 
 	for {
 		select {
-		case d := <-backendChannel:
-			fileLog, err := createLog(c.fileCount)
-			if err != nil {
-				fmt.Println(err)
-			}
+		case d,ok := <-backendChannel:
+			if ok {
+				fileLog, err := createLog(c.fileCount)
+				if err != nil {
+					fmt.Println(err)
+				}
 
-			if _, err = fileLog.Write(d.readData); err != nil {
-				fmt.Println(err.Error())
+				if _, err = fileLog.Write(d.readData); err != nil {
+					fmt.Println(err.Error())
+				}
+				fileLog.Close()
+				c.addUp(d.countData)
 			}
-			fileLog.Close()
-			c.addUp(d.countData)
 		}
 	}
 }
@@ -236,7 +225,7 @@ func (c *counter) dumpCounts() {
 	c.total = 0
 	c.totalSince = 0
 	c.totalUniq = 0
-	//c.duplicates = 0
+	c.duplicates = 0
 	/*file := c.memoizeFile()
 	file.Close()
 	fileLog, err := createLog(c.fileCount)
@@ -253,61 +242,57 @@ func (c *counter) dumpCounts() {
 
 func (c *counter) addUp(currentCounter *counter) {
 	c.mu.Lock()
-	c.total += currentCounter.total
-	c.duplicates += currentCounter.duplicates
-	for k, _ := range currentCounter.currentMap {
-		if c.currentMap[k] == 0 {
-			c.currentMap[k] = currentCounter.currentMap[k]
-			c.totalUniq++
-		} else {
-			c.currentMap[k] += currentCounter.currentMap[k]
-			c.duplicates++
+		c.total += currentCounter.total
+		c.duplicates += currentCounter.duplicates
+		c.totalSince += currentCounter.totalSince
+		for k, n := range currentCounter.currentMap {
+			if _,ok := c.currentMap[k];!ok {
+				c.totalUniq++
+			}else{
+				c.duplicates++
+			}
+			c.currentMap[k] += n
 		}
-	}
 	c.mu.Unlock()
 }
 
-func (c *counter) interval5() {
-	//do read out for uniq numbers in this interval of 10 seconds
+func (c *counter) interval5(connCount *count,triggerInterval5Chan triggerChan) {
+	//do read out for uniq numbers in this interval of 5 seconds
 	// do read out for uniq numbers total for server duration
-	go func() {
-		c.countAllLogs()
-		fmt.Printf("Total unique numbers this session: %d"+
-			"\nTotal unique numbers: %d"+
-			"\nTotal duplicates received: %d"+
-			"\nTotal Recieved %d\n\n",
-			c.totalSince, c.totalUniq, c.duplicates, c.total)
-		c.dumpCounts()
-		time.Sleep(5 * time.Second)
-		c.interval5()
-	}()
+	for {
+		select{
+		case <- time.Tick(5 * time.Second):
+			go c.countAllLogs()
+			var i count = 0
+			valueCont := *connCount
+			for ;i < valueCont;i++{
+					triggerInterval5Chan <- 1
+			}
+			fmt.Printf("Total unique numbers this session: %d"+
+				"\nTotal unique numbers: %d"+
+				"\nTotal duplicates received: %d"+
+				"\nTotal Recieved %d\n\n",
+				c.totalSince, c.totalUniq, c.duplicates, c.total)
+			 c.dumpCounts()
+		}
+	}
 }
 
 func (c *counter) interval10() {
 	//Every 10 seconds, the log should rotate and increment the number in the name,
 	//all while only writing unique numbers. Example: data.0.log -> data.1.log -> data.2.log.
-	go func() {
-		c.mu.Lock()
-		c.fileCount++ //count and create new file
-		/*file := c.memoizeFile()
-		file.Close()
-		fileLog, err := createLog(c.fileCount)
-		if err != nil || c.fileCount > 10 {
-			return
+	for {
+		select{
+		case <- time.Tick(10 * time.Second):
+			c.mu.Lock()
+			c.fileCount++ //count and create new file
+			_, err := createLog(c.fileCount)
+			if err != nil || c.fileCount > 10 {
+				return
+			}
+			c.mu.Unlock()
 		}
-		c.memoizeFile = func() (*os.File) {
-			return fileLog
-		}*/
-		_, err := createLog(c.fileCount)
-		if err != nil || c.fileCount > 10 {
-			return
-		}
-		c.mu.Unlock()
-
-		time.Sleep(10 * time.Second)
-		c.interval10()
-	}()
-
+	}
 }
 
 func (c *counter) countAllLogs() {
@@ -328,10 +313,12 @@ func (c *counter) countAllLogs() {
 				fmt.Println(err)
 				continue
 			}
+			totalInFile := bytes.Count(data, []byte("\n"))
+			c.total += totalInFile
 			if strings.Contains(value, string(c.fileCount)) {
-				c.totalSince += bytes.Count(data, []byte("\n"))
+				c.totalSince += totalInFile
 			} else {
-				c.totalUniq += bytes.Count(data, []byte("\n"))
+				c.totalUniq += totalInFile
 			}
 
 			logFile.Close()
